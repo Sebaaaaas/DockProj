69e74da27d86fc9f9ef67895550f14da4dc4c58c
Sebaaaaas <sebsan01@ucm.es>
2024-12-07 18:36:41 +0100

    Blug + water cool

--- Docktun/Assets/scripts/scroll_texture.cs
+++ Docktun/Assets/scripts/scroll_texture.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,34 @@ public class scroll_texture : MonoBehaviour
 {
     public float scroll_speed_x = 0.5F;
     public float scroll_speed_y = 0.5F;
+    public float side_to_side_time = 3.0F;
+    float timer = 0F;
+    bool forward = false;
+    private Vector2 currentOffset;
 
     private MeshRenderer my_renderer;
     void Start()
     {
         my_renderer = GetComponent<MeshRenderer>();
+        currentOffset = Vector2.zero;
     }
     void Update()
     {
         my_renderer.material.mainTextureOffset = new Vector2(scroll_speed_x * Time.realtimeSinceStartup, scroll_speed_y * Time.realtimeSinceStartup);
+
+        timer += Time.deltaTime;
+        if(timer > side_to_side_time)
+        {
+            forward = !forward;
+            timer -= side_to_side_time;
+        }
+
+        // Aceleramos cuando estamos en medio del movimiento, y movemos en funcion de forwards
+        float delta = Time.deltaTime * scroll_speed_x * (forward ? 1 : -1);
+        currentOffset.x += delta * (side_to_side_time / 2 - timer);       
+        currentOffset.y += delta * (side_to_side_time / 2 - timer); 
+
+        // Acceso al material en el albedo
+        my_renderer.material.SetTextureOffset("_DetailAlbedoMap", currentOffset);
     }
 }
